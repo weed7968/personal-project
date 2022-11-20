912060ab8de4673c2cb033d1354d6b1f510aa7e6
weed7968 <97829172+weed7968@users.noreply.github.com>
2022-11-20 15:10:06 +0900

    game1

---
+++ src/game1/Tic-tac-toe.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./Board";
+
+function Tic() {
+  const handleClick = () => {};
+
+  const jumpTo = () => {};
+
+  const renderMoves = () => {};
+
+  return <Board onclick={handleClick} />;
+}
+
+export default Tic;
---
+++ src/mainpage/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MainPage.css";
+import Tic from "../game1/Tic-tac-toe";
+
+function MainPage() {
+  return (
+    <div>
+      테스트
+      <Tic />
+    </div>
+  );
+}
+
+export default MainPage;
